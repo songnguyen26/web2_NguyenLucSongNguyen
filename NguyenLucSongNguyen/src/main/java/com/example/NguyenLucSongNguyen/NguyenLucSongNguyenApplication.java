38faf6178bfb8f3f91ca8775a9d6df47ea971fd3
NguyenLucSongNguyen <lucsongnguyen99@gmail.com>
2025-02-24 10:25:02 +0700

    firts commit

---
+++ NguyenLucSongNguyen/src/main/java/com/example/NguyenLucSongNguyen/NguyenLucSongNguyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenLucSongNguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenLucSongNguyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenLucSongNguyenApplication.class, args);
+	}
+
+}
---
+++ NguyenLucSongNguyen/src/test/java/com/example/NguyenLucSongNguyen/NguyenLucSongNguyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenLucSongNguyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenLucSongNguyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
