33522f89ac3720738e165eb43d8beb083f1799cd
NguyenLucSongNguyen <lucsongnguyen99@gmail.com>
2025-03-17 21:46:30 +0700

    buoi4

---
+++ NguyenLucSongNguyen/src/main/java/com/example/NguyenLucSongNguyen/domain/Brand.java
@@ -0,0 +1,7 @@
+package com.example.NguyenLucSongNguyen.domain;
+
+public class Brand {
+    private int id;
+    private String brandName;
+    private String image; 
+}
---
+++ NguyenLucSongNguyen/src/main/java/com/example/NguyenLucSongNguyen/domain/Category.java
@@ -0,0 +1,7 @@
+package com.example.NguyenLucSongNguyen.domain;
+
+public class Category {
+    private int categoryId;
+    private String categoryName;
+    private String image;
+}
